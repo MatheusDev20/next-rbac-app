24b9532a9c88d642b405ae5129d1471d3f858784
matheus <matheusdev20@gmail.com>
2024-05-30 02:18:17 -0300

    feat: first permissions

---
+++ apps/api/src/index.ts
@@ -0,0 +1,1 @@
+import { ability } from '@sass/auth'
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,3 +1,28 @@
-const tst = 'oi'
+import {
+  AbilityBuilder,
+  CreateAbility,
+  createMongoAbility,
+  ForcedSubject,
+  MongoAbility,
+} from '@casl/ability'
 
-console.log('Tst', tst)
+const actions = ['manager', 'invite', 'delete'] as const
+const subjects = ['User', 'all'] as const
+
+type AppAbilities = [
+  (typeof actions)[number],
+  (
+    | (typeof subjects)[number]
+    | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
+  ),
+]
+
+export type AppAbility = MongoAbility<AppAbilities>
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
+
+const { can, cannot, build } = new AbilityBuilder(createAppAbility)
+
+can('invite', 'User')
+cannot('delete', 'User')
+
+export const ability = build()
