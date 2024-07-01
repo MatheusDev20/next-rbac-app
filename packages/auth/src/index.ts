89d86af8a820da528a1a74c6729ff96df5fc4dcc
matheus <matheusdev20@gmail.com>
2024-06-30 22:24:55 -0300

    casl permissions

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -2,27 +2,28 @@ import {
   AbilityBuilder,
   CreateAbility,
   createMongoAbility,
-  ForcedSubject,
   MongoAbility,
 } from '@casl/ability'
 
-const actions = ['manager', 'invite', 'delete'] as const
-const subjects = ['User', 'all'] as const
+import { User } from './models/user'
+import { permissions } from './permissions'
+import { ProjectSubject } from './subjects/project'
+import { UserSubject } from './subjects/user'
 
-type AppAbilities = [
-  (typeof actions)[number],
-  (
-    | (typeof subjects)[number]
-    | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
-  ),
-]
+type AppAbilities = UserSubject | ProjectSubject | ['manage', 'all']
 
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
 
-const { can, cannot, build } = new AbilityBuilder(createAppAbility)
+export function defineAbilityFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility)
 
-can('invite', 'User')
-cannot('delete', 'User')
+  if (typeof permissions[user.role] !== 'function')
+    throw new Error(`Unknown role: ${user.role}`)
 
-export const ability = build()
+  permissions[user.role](user, builder)
+
+  const ability = builder.build()
+
+  return ability
+}
